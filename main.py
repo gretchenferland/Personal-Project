4a1df30e5423b955cb5883d96060568023f67310
gretchenferland <54412513+gretchenferland@users.noreply.github.com>
2019-10-20 23:49:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 # Gretchen Ferland
 # trivia game
 
-print("welcome to the states test")
+def printMessage():
+    print("Welcome to the school game")
+    print("Lets learn some new things!")
 
+def main():
+    print("Hello Student")
+    printMessage()
+
+main()
+
+print("lets look at some simple math equations first ")
+
+print("a will equal 2")
+print("b will equal 5")
 a= int(2)
 b= int(5)
+print("a-b")
 print(a-b)
+print("b*b")
 print(b*b)
+print("b/a")
 print(b/a)
+
+print("now lets look at a more complicated problem")
+import math
+
+def getQuadratic(a,b) :
+    square = a**2 + b**2
+    squareRoot = math.sqrt(square)
+    return squareRoot
+
+def main():
+    print("The sqaure of the sum of the squares of 3 and 4 is:", getQuadratic(3,4))
+
+main()
